6def9b3922fd0b81bdf6287088f4b41b19478fa5
Thom204 <tmonnier@unal.edu.co>
2024-07-29 00:31:47 -0500

    ej 2

--- Actividad3/Actividad3_p1/src/Ej_PN19/Ej_PN19.java
+++ Actividad3/Actividad3_p1/src/Ej_PN19/Ej_PN19.java
@@ -1,5 +1,26 @@
 package Ej_PN19;
 import java.util.Scanner;
 public class Ej_PN19 {
-    
+    public static void main(String[] args) {
+        double lado;
+        double perimetro, altura, area;
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Ingrese el lado del Triangulo: ");
+        lado = entrada.nextDouble();
+
+        Triangulo T = new Triangulo(lado);
+
+        perimetro = T.perimetro();
+
+        altura = T.altura();
+
+        area = T.area();
+
+        System.out.println("El perimetro del Triangulo es: " + perimetro);
+        System.out.println("La altura del Triangulo es: " + altura);
+        System.out.println("El area del Triangulo es: " + area);
+        entrada.close();
+    }
 }
---
+++ Actividad3/Actividad3_p1/src/Ej_PN19/Triangulo.java
@@ -0,0 +1,20 @@
+
+package Ej_PN19;
+
+
+public class Triangulo {
+    double lado;
+
+    public Triangulo(double _lado) {
+        this.lado = _lado;
+    }
+    double perimetro() {
+        return lado * 3;
+    }
+    double altura() {
+        return (Math.sqrt(3) / 2) * lado;
+    }
+    double area() {
+        return (Math.sqrt(3)  * Math.pow(lado,2))/4;
+    }
+}
