d589f58f9ec9d14b3cf688adcb516f5fd0041689
Thom204 <tmonnier@unal.edu.co>
2024-09-24 23:59:25 -0500

    Update window.java

--- Actividad_6/src/actividad_6/window.java
+++ Actividad_6/src/actividad_6/window.java
@@ -133,7 +133,7 @@ public class window extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
+        this.show(false);
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
